00729974e6ea3a91d9ded70b0298cf89711e1ed2
Minhaj Islam <minhajislam674@gmail.com>
2023-03-24 18:05:39 +0100

    Changed the app logo

--- src/services/image.url.ts
+++ src/services/image.url.ts
@@ -1,11 +1,3 @@
-// const getCroppedImageUrl = (url: string) => {
-//   const target = "media/";
-//   const index = url.indexOf(target) + target.length;
-//   return url.slice(0, index) + "crop/600/400" + url.slice(index);
-// };
-
-// export default getCroppedImageUrl;
-
 import noImage from "../assets/no-image.jpg";
 
 const getCroppedImageUrl = (url: string | null) => {
--- src/theme.ts
+++ src/theme.ts
@@ -1,12 +1,32 @@
 // 1. import `extendTheme` function
 import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+import { color } from "framer-motion";
 
 // 2. Add your color mode config
 const config = {
   initialColorMode: "light",
+  useSystemColorMode: false,
+};
+
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+  gray: {
+    50: "#f5f5f5",
+  },
+};
+
+//add fonts
+
+const fonts = {
+  heading: "Roboto",
+  body: "Roboto",
 };
 
 // 3. extend the theme
-const theme = extendTheme({ config });
+const theme = extendTheme({ config, colors, fonts });
 
 export default theme;
